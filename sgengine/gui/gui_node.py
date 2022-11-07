c4c0255f9b956084e198aa3da3200c80bf01f448
Justin Davis <davis.justin@mssm.org>
2022-11-07 16:42:59 -0700

    example of Oak node accesible at high level

--- sgengine/gui/gui_node.py
+++ sgengine/gui/gui_node.py
@@ -1,27 +1,14 @@
 from ..abstract_node import AbstractNode
 
-from std_msgs import String
-
 
 class GUINode(AbstractNode):
     """The node for updating the GUI."""
 
     def __init__(self, name: str, *args, **kwargs) -> None:
         super().__init__(name, *args, **kwargs)
-
         # TODO: define web server stuff and whatevers
         pass
 
     def _main(self):
         # TODO: handle opening a web server and publishing stuff to it
-        self.publish("testing", self._testing_generator, String, 10)
-
-        self.subscribe("testing", self._testing_callback, String)
         pass
-
-    def _testing_generator(self):
-        while True:
-            yield String(data="testing")
-
-    def _testing_callback(self, msg: String):
-        print(msg.data)
--- sgengine/pathfinding/pathfinding_node.py
+++ sgengine/pathfinding/pathfinding_node.py
@@ -6,11 +6,9 @@ class PathfindingNode(AbstractNode):
 
     def __init__(self, name: str, *args, **kwargs) -> None:
         super().__init__(name, *args, **kwargs)
-        self._world: Any = None
-        self._path: List[Any] = []
-        self._path_publisher: Optional[Publisher] = None
-        self._world_subscriber: Optional[Subscription] = None
+        # TODO
+        pass
 
     def _main(self):
-        self._path_publisher = self.publish("path", [])
-        self._world_subscriber = self.subscribe("world", self._world_callback, World)
+        # TODO
+        pass
