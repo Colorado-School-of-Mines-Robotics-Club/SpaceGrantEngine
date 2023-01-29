ef73fbbfde27311db12e4c92085291e6709f3d5b
Ethan Stacy <ethanstacy02@gmail.com>
2023-01-29 16:55:52 -0700

    Add gui node code

--- sgengine/gui/gui_node.py
+++ sgengine/gui/gui_node.py
@@ -1,14 +1,26 @@
+from flask import Flask
+
 from ..abstract_node import AbstractNode
 
 
 class GUINode(AbstractNode):
     """The node for updating the GUI."""
 
+    app = Flask("Remote Interface")
+
+    @app.route("/")
+    def _home(self):
+        # Display home page of server
+        return "Hello World!"
+
     def __init__(self, name: str, *args, **kwargs) -> None:
         super().__init__(name, *args, **kwargs)
         # TODO: define web server stuff and whatevers
+        print("Init")
         pass
 
     def _main(self):
         # TODO: handle opening a web server and publishing stuff to it
+        print("Run")
+        self.app.run()
         pass
