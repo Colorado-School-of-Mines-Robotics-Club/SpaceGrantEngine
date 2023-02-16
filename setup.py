710009e41a617cb9e465fee6c5cb55667d17b10d
justincdavis <davis.justin@mssm.org>
2023-02-16 14:28:21 -0700

    removed oak entry point, use depthai-ros-driver

--- setup.py
+++ setup.py
@@ -35,7 +35,6 @@ setup(
     tests_require=["pytest"],
     entry_points={
         "console_scripts": [
-            "oak = sgengine.hardware.oakds2.oakds2_node:main",
             "test = sgengine.test_node:main",
         ],
     },
