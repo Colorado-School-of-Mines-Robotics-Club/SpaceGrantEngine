0b97f640bc09ebcbd9e6f41569b91168a66974b8
justincdavis <jcdavis@mssm.org>
2023-04-13 14:32:34 -0600

    Removed testing files from segmentation, began odometry node

--- sgengine/sgengine/odometry/odometry_node.py
+++ sgengine/sgengine/odometry/odometry_node.py
@@ -1,11 +1,22 @@
-from ..abstract_node import AbstractNode
+# pylint: skip-file
+
+import sys
+import time
+import rclpy
+
+# from std_msgs.msg import String
+from sgengine_messages.msg import TwoFloat
+from rclpy.node import Node
+f
 
 
-class OdometryNode(AbstractNode):
+class OdometryNode(Node):
     """Node for running visual odometry"""
 
-    def __init__(self, name: str, *args, **kwargs) -> None:
-        super().__init__(name, *args, **kwargs)
+    def __init__(self) -> None:
+        Node.__init__(self, "odometer")
+
+        self._publisher = self.create_publisher(, "odometer/tvec", 10)
 
         # TODO: attributes
         pass
