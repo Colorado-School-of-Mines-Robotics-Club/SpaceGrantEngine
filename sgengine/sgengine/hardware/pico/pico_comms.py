318430ac74742b40e8a1f0d485a2da1bf7c94348
Ethan Stacy <ethanstacy02@gmail.com>
2023-11-12 14:39:12 -0700

    Change to dedicated Manual control node

--- sgengine/sgengine/hardware/pico/pico_comms.py
+++ sgengine/sgengine/hardware/pico/pico_comms.py
@@ -15,7 +15,7 @@ except RuntimeError as e:
 class PicoComms:
     """Class to manage serial communication with Picos"""
 
-    def __init__(self, serial_port="/dev/ttyS0", interrupt_pin=5, baud=112500):
+    def __init__(self, serial_port="/dev/ttyS0", interrupt_pin=5, baud=38400):
         """Creates a cummunication line to send instructions to the pi pico.
         The interrupt pin is the pin that will be used to tell the pico that it has received an instruction
         """
@@ -44,10 +44,10 @@ class PicoComms:
         GPIO.setup(interrupt_pin, GPIO.OUT)
         GPIO.output(interrupt_pin, 0)
 
-    def send_move_command(self, angular: float, linear: float):
-        """sends an instruction consisting of speed and direction to pico"""
+    def send_move_command(self, left: int, right: int):
+        """sends an instruction consisting of a left and right motor speed to pico"""
         # format
-        self.send_str_direct(str(angular) + "," + str(linear))
+        self.send_str_direct(str(left) + "," + str(right) + '\0')
 
     def send_str_direct(self, msg: str) -> None:
         """Directly sends string message to pico"""
