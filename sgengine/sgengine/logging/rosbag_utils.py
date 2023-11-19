f3350bafe5628a12d8c5789f37c2532a8734bf98
CailynSmith <42589127+ccsmith03@users.noreply.github.com>
2023-11-19 20:59:53 +0000

    Add functionality for publishing data from a rosbag

--- sgengine/sgengine/logging/rosbag_utils.py
+++ sgengine/sgengine/logging/rosbag_utils.py
@@ -1,5 +1,6 @@
 import rosbag
 import rclpy
+import sys
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from odometry.odometry_node import OdometryNode
@@ -13,9 +14,11 @@ class RosbagUtils(Node):
         Node.__init__(self, "rosbag_utils")
         self._filename = filename
         self._bag = rosbag.Bag(self._filename, 'w')
+    
+    def write_bag(self):
         self._pose_subscriber = self.create_subscription(RPYXYZ, "/odom/rpy_xyz", self.save_pose, 10)
-        self._depth_subscriber = self.create_publisher(Image, "/odom/depth", self.save_depth, 10)
-        self._rgb_subscriber = self.create_publisher(Image, "/odom/rgb", self.save_rgb, 10)
+        self._depth_subscriber = self.create_subscription(Image, "/odom/depth", self.save_depth, 10)
+        self._rgb_subscriber = self.create_subscription(Image, "/odom/rgb", self.save_rgb, 10)
     
     def save_pose(self, pose: RPYXYZ):
         try:
@@ -38,15 +41,46 @@ class RosbagUtils(Node):
     def close_bag(self):
         self._bag.close()
 
-def main(args=None):
-    """Main function which launches the RosbagUtils node and Odometry node."""
+    def read_bag(self):
+        self._pose_publisher = self.create_publisher(RPYXYZ, "/odom/rpy_xyz", 10)
+        self._depth_publisher = self.create_subscription(Image, "/odom/depth", self.save_depth, 10)
+        self._rgb_publisher = self.create_subscription(Image, "/odom/rgb", self.save_rgb, 10)
+
+        for topic, msg in self._bag.read_messages(topics=['pose', 'depth', 'rgb']):
+            if topic == '/odom/rpy_xyz':
+                self._pose_publisher.publish(msg)  # Publish to pose topic
+            elif topic == '/odom/depth':
+                self._depth_publisher.publish(msg)  # Publish to depth image topic
+            else:
+                self._rgb_publisher.publish(msg)  # Publish to rgb image topic
+
+def main(args=None, read=True):
+    """Main function which launches the RosbagUtils node and Odometry node.
+    If reading from the rosbag, start another node that reads from the topics it publishes to."""
     rclpy.init(args=args)
-    odometer = OdometryNode()
-    rosbag_utils = RosbagUtils()
-    rclpy.spin(odometer)
-    rclpy.spin(rosbag_utils)
-    rosbag_utils.close_bag()
-    odometer.destroy_node()
+    filename = "odometry_data.bag"
+    rosbag_utils = RosbagUtils(filename=filename)
+    if read:
+        rosbag_utils.read_bag()
+        rclpy.spin(rosbag_utils)  # Keep publishing until shut down
+        rosbag_utils.close_bag()
+    else:
+        odometer = OdometryNode()
+        rosbag_utils.write_bag()
+        rclpy.spin(odometer)
+        rclpy.spin(rosbag_utils)  # Keep subscribing until shut down
+        rosbag_utils.close_bag()
+        odometer.destroy_node()
     rosbag_utils.destroy_node()
     rclpy.shutdown()
 
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python rosbag_utils.py [read/write]")
+        sys.exit(1)
+
+    mode = sys.argv[1]
+    if mode == 'read':
+        main(read=True)
+    else:
+        main(read=False)
