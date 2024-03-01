1dd85fc971b3655a9061b4a9686697c2194ff0d4
Ethan Stacy <ethanstacy02@gmail.com>
2024-03-01 15:04:40 -0700

    Update default logging path

--- sgengine/sgengine/sg_logger.py
+++ sgengine/sgengine/sg_logger.py
@@ -5,10 +5,10 @@ import os
 class SG_Logger:
 
     # constructor to setup the logging for all the nodes
-    # when classes inherit this one, logs go to a file(important_logs.log) and to the stderr console
+    # when classes inherit this one, logs go to a file(sg_logs.log) and to the stderr console
     def __init__(self) -> None:
         logging.basicConfig(
-            filename=os.getenv("SG_LOG_PATH", "log/important_logs.log"),
+            filename=os.getenv("SG_LOG_PATH", "log/sg_logs.log"),
             level=logging.INFO,
             format="%(levelname)s:%(filename)s:%(lineno)d (@%(asctime)s) %(message)s",
         )
